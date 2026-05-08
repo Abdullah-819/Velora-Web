68ac0d91bb4c92e6e613116d08e8b29915a35c8b
Abdullah Rana <ranaabdullah228.ar@gmail.com>
2026-05-08 16:12:51 +0500

    refactor: create barrel file for components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as AdminSidebar } from './AdminSidebar';
+export { default as AdminTopbar } from './AdminTopbar';
+export { default as AppBootLoader } from './AppBootLoader';
+export { default as Loader } from './Loader';
+export { default as PanelCard } from './PanelCard';
+export { default as StatCard } from './StatCard';
